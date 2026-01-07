3fad0fcba1e7c125fb50c59199b9b979d54e4c76
paraspatankar <patankarparas@gmail.com>
2026-01-07 20:15:16 +0530

    Update app/dashboard/settings/page.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -8,7 +8,7 @@ const SettingPage = () => {
   return (
     <div className="space-y-6">
       <div className="text-primary">
-        <h1 className="text-3xl sm:text-4x1 font-bold tracking-tight">
+        <h1 className="text-3xl sm:text-4xl font-bold tracking-tight">
           Settings
         </h1>
         <p className="text-muted-foreground mt-1">
