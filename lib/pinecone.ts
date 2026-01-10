d585629ad3363664227946b65ce51939b350f415
paras-patankar <patankarparas@gmail.com>
2026-01-10 15:34:20 +0530

    09 Inngest Pinecone aisdk init-1

---
+++ lib/pinecone.ts
@@ -0,0 +1,9 @@
+import { Pinecone } from "@pinecone-database/pinecone";
+
+export const pinecone = new Pinecone({
+  apiKey: process.env.PINECONE_DB_API_KEY!,
+  //environment: process.env.PINECONE_ENVIRONMENT!,
+});
+
+// Get the index
+export const pineconeIndex = pinecone.index("codebunny-vector-embeddings-v1");
